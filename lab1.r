fdc87131af28303e729a8da59b2eaedc17269959
Franco <franco.leal@usach.cl>
2018-11-12 20:24:19 -0300

    plots continuas

--- lab1.r
+++ lab1.r
@@ -1,11 +1,12 @@
 library(ggplot2)
 
-path = "~/Escritorio/USACH/Analisis de Datos/Lab1/hepatitis.data"
+#path = "~/Escritorio/USACH/Analisis de Datos/Lab1/hepatitis.data"
+path = "~/Documentos/AnalisisDatosLab1/hepatitis.data"
 hepatitis <- read.table(path,sep=",", na.strings = c("?"))
 
 names <- c("Class","AGE","SEX","STEROID","ANTIVIRALS","FATIGUE","MALAISE",
-           "ANOREXIA","LIVER BIG","LIVER FIRM","SPLEEN PALPABLE","SPIDERS",
-           "ASCITES","VARICES","BILIRUBIN","ALK PHOSPHATE","SGOT","ALBUMIN",
+           "ANOREXIA","LIVER_BIG","LIVER_FIRM","SPLEEN_PALPABLE","SPIDERS",
+           "ASCITES","VARICES","BILIRUBIN","ALK_PHOSPHATE","SGOT","ALBUMIN",
            "PROTIME","ISTOLOGY")
 
 colnames(hepatitis) <- names
@@ -22,3 +23,21 @@ medians <- sapply(hepatitis.without.na,median)
 modes <- sapply(hepatitis.without.na,getmode)
 vars <- sapply(hepatitis.without.na,var)
 
+p.1 <- ggplot(hepatitis.without.na, aes(x=NULL, y=AGE)) + geom_boxplot()
+
+p.2 <- ggplot(hepatitis.without.na, aes(x=NULL, y=BILIRUBIN)) + geom_boxplot()
+
+p.3 <- ggplot(hepatitis.without.na, aes(x=NULL, y=ALK_PHOSPHATE)) + geom_boxplot()
+
+p.4 <- ggplot(hepatitis.without.na, aes(x=NULL, y=SGOT)) + geom_boxplot()
+
+p.5 <- ggplot(hepatitis.without.na, aes(x=NULL, y=ALBUMIN)) + geom_boxplot()
+
+p.6 <- ggplot(hepatitis.without.na, aes(x=NULL, y=PROTIME)) + geom_boxplot()
+
+show(p.1)
+show(p.2)
+show(p.3)
+show(p.4)
+show(p.5)
+show(p.6)
